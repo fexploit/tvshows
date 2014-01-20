5e3740012017fdfe885e9490a2dcfa57de2a44e9
senoldinc <senol.dinc@windowslive.com>
2014-01-20 13:20:01 +0200

    project is created

---
+++ Izlemag.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Izlemag.Web.MvcApplication" Language="C#" %>
---
+++ Izlemag.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Izlemag.Web.Startup))]
+namespace Izlemag.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
