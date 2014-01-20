6b8a6caaee2732573d93625095d92d605975d87a
senoldinc <senol.dinc@windowslive.com>
2014-01-20 15:01:45 +0200

    add data layer

---
+++ Izlemag.DataAccess/DbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Izlemag.Data;
+using System.Data.Entity;
+
+namespace Izlemag.DataAccess
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+    public class DbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbContext() 
+            : base("name=izlemag")
+        {
+
+        }
+
+        public DbSet<Episode> Episodes { get; set; }
+        public static DbSet<Season> Seasons { get; set; }
+        public static DbSet<Tag> Tags { get; set; }
+        public static DbSet<TvShow> TvShows { get; set; }
+    }
+}
